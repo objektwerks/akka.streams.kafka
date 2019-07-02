8f54b600fc9b5524349d1fb43517834abdb594c1
objektwerks <objektwerks@runbox.com>
2019-07-02 06:35:36 -0400

    refactored kafka akka stream test

--- src/test/scala/kafka/KafkaAkkaStreamTest.scala
+++ src/test/scala/kafka/KafkaAkkaStreamTest.scala
@@ -6,9 +6,8 @@ import akka.Done
 import akka.actor.ActorSystem
 import akka.kafka.scaladsl.{Consumer, Producer}
 import akka.kafka.{ConsumerSettings, ProducerSettings, Subscriptions}
-import akka.stream.Supervision.Decider
+import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Sink, Source}
-import akka.stream.{ActorMaterializer, ActorMaterializerSettings, Supervision}
 import com.typesafe.config.ConfigFactory
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerRecord
@@ -25,12 +24,20 @@ class KafkaAkkaStreamTest extends FunSuite with BeforeAndAfterAll with Matchers 
   val config = ConfigFactory.load("test.conf")
   val producerConfig = config.getConfig("akka.kafka.producer")
   val consumerConfig = config.getConfig("akka.kafka.consumer")
-  implicit val system = ActorSystem.create("stream", config)
+
+  implicit val system = ActorSystem.create("kafka-akka-stream", config)
+  implicit val materializer = ActorMaterializer()
   implicit val dispatcher = system.dispatcher
-  val decider: Decider = Supervision.restartingDecider
-  val settings = ActorMaterializerSettings(system).withSupervisionStrategy(decider)
-  implicit val materializer = ActorMaterializer(settings)
+
   val topic = "kv"
+
+  val producerSettings = ProducerSettings(producerConfig, new StringSerializer, new StringSerializer)
+    .withBootstrapServers(producerConfig.getString("bootstrap.servers"))
+
+  val consumerSettings = ConsumerSettings(consumerConfig, new StringDeserializer, new StringDeserializer)
+    .withBootstrapServers(consumerConfig.getString("bootstrap.servers"))
+    .withGroupId(consumerConfig.getString("group.id"))
+    .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, consumerConfig.getString("auto.offset.reset"))
 
   override protected def afterAll(): Unit = {
     Await.result(system.terminate(), 1 second)
@@ -43,35 +50,31 @@ class KafkaAkkaStreamTest extends FunSuite with BeforeAndAfterAll with Matchers 
   }
 
   def produceMessages(count: Int): Unit = {
-    val settings = ProducerSettings(producerConfig, new StringSerializer, new StringSerializer)
-      .withBootstrapServers(producerConfig.getString("bootstrap.servers"))
     val done = Source(1 to count)
       .map(_.toString)
-      .map { s =>
-        val record = new ProducerRecord[String, String] (topic, s, s)
+      .map { string =>
+        val record = new ProducerRecord[String, String] (topic, string, string)
         logger.info(s"Producer -> key: ${record.key} value: ${record.value}")
         record
       }
-      .runWith(Producer.plainSink(settings))
+      .runWith(Producer.plainSink(producerSettings))
     Await.result(done, 3 seconds)
     ()
   }
 
   def consumeMessages(): Int = {
-    val settings = ConsumerSettings(consumerConfig, new StringDeserializer, new StringDeserializer)
-      .withBootstrapServers(consumerConfig.getString("bootstrap.servers"))
-      .withGroupId(consumerConfig.getString("group.id"))
-      .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, consumerConfig.getString("auto.offset.reset"))
     val count = new AtomicInteger()
-    Consumer.committableSource(settings, Subscriptions.topics(topic))
-      .mapAsync(1) { message =>
+    val done = Consumer
+      .committableSource(consumerSettings, Subscriptions.topics(topic))
+      .mapAsync(parallelism = 1) { message =>
+        val record = message.record
         count.incrementAndGet
-        logger.info(s"Consumer -> key: ${message.record.key} value: ${message.record.value}")
+        logger.info(s"Consumer -> topic: ${record.topic} partition: ${record.partition} offset: ${record.offset} key: ${record.key} value: ${record.value}")
         Future.successful(Done).map(_ => message)
       }
-      .mapAsync(1) { message => message.committableOffset.commitScaladsl() }
+      .mapAsync(parallelism = 1) { message => message.committableOffset.commitScaladsl() }
       .runWith(Sink.ignore)
-    Thread.sleep(9000L) // Future[Done] returned from Consumer never completes.
+    Await.result(done, 3 seconds)
     count.get
   }
 }
