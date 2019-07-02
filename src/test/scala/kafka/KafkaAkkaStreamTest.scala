8478b317cb26a49bc81f3b0686b24fd6d144b07f
objektwerks <objektwerks@runbox.com>
2019-07-02 08:59:11 -0400

    remove explicit future type

--- src/test/scala/kafka/KafkaAkkaStreamTest.scala
+++ src/test/scala/kafka/KafkaAkkaStreamTest.scala
@@ -78,7 +78,7 @@ class KafkaAkkaStreamTest extends FunSuite with BeforeAndAfterAll with Matchers 
     count.get
   }
 
-  def consumeMessage(message: CommittableMessage[String, String], count: AtomicInteger): Future[Done] = Future[Done] {
+  def consumeMessage(message: CommittableMessage[String, String], count: AtomicInteger): Future[Done] = Future {
     val record = message.record
     logger.info(s"*** Consumer -> topic: ${record.topic} partition: ${record.partition} offset: ${record.offset} key: ${record.key} value: ${record.value}")
     count.incrementAndGet
