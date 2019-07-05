05662366f71641d49cc581c8ff5a255b896e17f6
objektwerks <objektwerks@runbox.com>
2019-07-05 06:46:10 -0400

    added TestConf

--- src/test/scala/kafka/KafkaAkkaStreamTest.scala
+++ src/test/scala/kafka/KafkaAkkaStreamTest.scala
@@ -1,14 +1,11 @@
 package kafka
 
 import akka.actor.ActorSystem
+import akka.kafka.Subscriptions
 import akka.kafka.scaladsl.{Committer, Consumer, Producer}
-import akka.kafka.{CommitterSettings, ConsumerSettings, ProducerSettings, Subscriptions}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Keep, Source}
-import com.typesafe.config.ConfigFactory
-import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
 import org.scalatest.{BeforeAndAfterAll, FunSuite, Matchers}
 
 import scala.concurrent.Await
@@ -18,18 +15,12 @@ import scala.util.Try
 
 class KafkaAkkaStreamTest extends FunSuite with BeforeAndAfterAll with Matchers {
   import KafkaCommon._
-
-  val config = ConfigFactory.load("test.conf")
-  val producerConfig = config.getConfig("akka.kafka.producer")
-  val consumerConfig = config.getConfig("akka.kafka.consumer")
-  val committerConfig = config.getConfig("akka.kafka.committer")
+  import TestConf._
 
   implicit val system = ActorSystem.create("kafka-akka-stream", config)
   implicit val materializer = ActorMaterializer()
   implicit val dispatcher = system.dispatcher
   implicit val logger = system.log
-
-  val topic = "kv"
 
   override protected def afterAll(): Unit = {
     Await.result(system.terminate, 3 seconds)
@@ -50,9 +41,6 @@ class KafkaAkkaStreamTest extends FunSuite with BeforeAndAfterAll with Matchers 
   }
 
   def produceMessages(count: Int): Unit = {
-    val producerSettings = ProducerSettings[String, String](producerConfig, new StringSerializer, new StringSerializer)
-      .withBootstrapServers(producerConfig.getString("bootstrap.servers"))
-
     val done = Source(1 to count)
       .map(_.toString)
       .map { string =>
@@ -66,13 +54,6 @@ class KafkaAkkaStreamTest extends FunSuite with BeforeAndAfterAll with Matchers 
   }
 
   def consumeMessages(): Unit = {
-    val consumerSettings = ConsumerSettings[String, String](consumerConfig, new StringDeserializer, new StringDeserializer)
-      .withBootstrapServers(consumerConfig.getString("bootstrap.servers"))
-      .withGroupId(consumerConfig.getString("group.id"))
-      .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, consumerConfig.getString("auto.offset.reset"))
-
-    val committerSettings = CommitterSettings(committerConfig)
-
     val done = Consumer
       .committableSource(consumerSettings, Subscriptions.topics(topic))
       .map { message =>
@@ -83,7 +64,7 @@ class KafkaAkkaStreamTest extends FunSuite with BeforeAndAfterAll with Matchers 
       .toMat(Committer.sink(committerSettings))(Keep.right)
       // .mapMaterializedValue(DrainingControl.apply) // Somehow prevents message consume and commit ( with Keep.both above )
       .run
-    Try(Await.result(done, 3 seconds)) // Future[Done] never completes, so times out. But messages are consumed and committed.
+    Try(Await.result(done, 3 seconds)) // Future[Done] never completes, so times out. But messages are consumed and offsets committed.
     ()
   }
 }
--- src/test/scala/kafka/KafkaCommon.scala
+++ src/test/scala/kafka/KafkaCommon.scala
@@ -1,26 +1,16 @@
 package kafka
 
 import java.time.Duration
-import java.util.Properties
 import java.util.concurrent.atomic.AtomicInteger
 
 import akka.event.LoggingAdapter
-import org.apache.kafka.clients.admin.{AdminClient, AdminClientConfig, NewTopic}
+import org.apache.kafka.clients.admin.{AdminClient, NewTopic}
 import org.apache.kafka.clients.consumer.KafkaConsumer
 
 import scala.collection.JavaConverters._
-import scala.io.Source
 
 object KafkaCommon {
-  val kafkaConsumerProperties = loadProperties("/kafka-consumer.properties")
-  val adminClientProperties = new Properties()
-  adminClientProperties.setProperty(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
-
-  def loadProperties(file: String): Properties = {
-    val properties = new Properties()
-    properties.load(Source.fromInputStream(getClass.getResourceAsStream(file)).bufferedReader())
-    properties
-  }
+  import TestConf._
 
   def assertTopic(topic: String): Boolean = {
     val adminClient = AdminClient.create(adminClientProperties)
@@ -35,14 +25,14 @@ object KafkaCommon {
     val count = new AtomicInteger()
     for (i <- 1 to 2) {
       val records = consumer.poll(Duration.ofMillis(100L))
-      logger.info(s"**** Consumer -> { ${records.count} } records polled on attempt { $i }.")
+      logger.info(s"+++ Consumer -> { ${records.count} } records polled on attempt { $i }.")
       records.iterator.asScala.foreach { record =>
-        logger.info(s"**** Consumer -> topic: ${record.topic} partition: ${record.partition} offset: ${record.offset} key: ${record.key} value: ${record.value}")
+        logger.info(s"+++ Consumer -> topic: ${record.topic} partition: ${record.partition} offset: ${record.offset} key: ${record.key} value: ${record.value}")
         count.incrementAndGet()
       }
     }
     consumer.close()
-    logger.info(s"**** Consumer -> record count is ${count.get}")
+    logger.info(s"+++ Consumer -> record count is ${count.get}")
     count.get
   }
 }
