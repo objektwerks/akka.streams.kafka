19b2eae6057eea4f1c987c7af0318a0275e74e3f
objektwerks <objektwerks@runbox.com>
2023-04-12 09:48:20 -0400

    edited conf

--- src/main/scala/objektwerks/Conf.scala
+++ src/main/scala/objektwerks/Conf.scala
@@ -18,17 +18,19 @@ final class Conf extends EmbeddedKafka {
   val producerSettings = ProducerSettings[String, String](
     producerConfig,
     new StringSerializer,
-    new StringSerializer)
-    .withBootstrapServers(producerConfig.getString("bootstrap.servers"))
+    new StringSerializer
+  )
+  .withBootstrapServers(producerConfig.getString("bootstrap.servers"))
 
   val consumerConfig = config.getConfig("akka.kafka.consumer")
   val consumerSettings = ConsumerSettings[String, String](
     consumerConfig,
     new StringDeserializer,
-    new StringDeserializer)
-    .withBootstrapServers(consumerConfig.getString("bootstrap.servers"))
-    .withGroupId(consumerConfig.getString("group.id"))
-    .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, consumerConfig.getString("auto.offset.reset"))
+    new StringDeserializer
+  )
+  .withBootstrapServers(consumerConfig.getString("bootstrap.servers"))
+  .withGroupId(consumerConfig.getString("group.id"))
+  .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, consumerConfig.getString("auto.offset.reset"))
 
   val committerConfig = config.getConfig("akka.kafka.committer")
   val committerSettings = CommitterSettings(committerConfig)
