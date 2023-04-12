fb426e4736ca04063d4d30d7e14e29d48c3627b9
objektwerks <objektwerks@runbox.com>
2023-04-12 08:39:01 -0400

    edited kafka

--- src/main/scala/objektwerks/Kafka.scala
+++ src/main/scala/objektwerks/Kafka.scala
@@ -2,18 +2,10 @@ package objektwerks
 
 import io.github.embeddedkafka._
 
-import java.time.Duration
-import java.util.Properties
-
 import org.slf4j.LoggerFactory
 import org.apache.kafka.common.serialization._
-import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.clients.consumer.KafkaConsumer
-import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.producer.KafkaProducer
-
-import scala.io.Source
-import scala.jdk.CollectionConverters._
 
 final class Kafka(conf: Conf) extends EmbeddedKafka {
   val logger = LoggerFactory.getLogger(getClass)
@@ -33,18 +25,5 @@ final class Kafka(conf: Conf) extends EmbeddedKafka {
     consumer.close()
     kafka.stop(false)
     logger.info("*** embedded kafka stopped")
-  }
-
-  def sendProducerRecord(topic: String, key: String, value: String): Unit = {
-    val record = new ProducerRecord[String, String](topic, key, value)
-    producer.send(record).get()
-    logger.info("*** producer send: {}", record)
-  }
- 
-  def pollConsumerRecords(topic: String): List[ConsumerRecord[String, String]] = {
-    consumer.subscribe( List(topic).asJava )
-    val records = consumer.poll( Duration.ofMillis(6000L) ).asScala.toList
-    logger.info("*** consumer poll: {}", records.size)
-    records
   }
 }
