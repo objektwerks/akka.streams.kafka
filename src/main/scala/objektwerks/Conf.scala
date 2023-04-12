3de156ae93d1b025081f08e0caa62674db80982c
objektwerks <objektwerks@runbox.com>
2023-04-12 08:54:18 -0400

    edited conf

--- src/main/scala/objektwerks/Conf.scala
+++ src/main/scala/objektwerks/Conf.scala
@@ -15,11 +15,17 @@ final class Conf extends EmbeddedKafka {
   val config = ConfigFactory.load("app.conf")
 
   val producerConfig = config.getConfig("akka.kafka.producer")
-  val producerSettings = ProducerSettings[String, String](producerConfig, new StringSerializer, new StringSerializer)
+  val producerSettings = ProducerSettings[String, String](
+    producerConfig,
+    new StringSerializer,
+    new StringSerializer)
     .withBootstrapServers(producerConfig.getString("bootstrap.servers"))
 
   val consumerConfig = config.getConfig("akka.kafka.consumer")
-  val consumerSettings = ConsumerSettings[String, String](consumerConfig, new StringDeserializer, new StringDeserializer)
+  val consumerSettings = ConsumerSettings[String, String](
+    consumerConfig,
+    new StringDeserializer,
+    new StringDeserializer)
     .withBootstrapServers(consumerConfig.getString("bootstrap.servers"))
     .withGroupId(consumerConfig.getString("group.id"))
     .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, consumerConfig.getString("auto.offset.reset"))
