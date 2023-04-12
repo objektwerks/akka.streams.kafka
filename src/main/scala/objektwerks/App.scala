6757624c7e23ab6ab49c9c47e5cfee48911adf20
objektwerks <objektwerks@runbox.com>
2023-04-12 11:45:15 -0400

    enhanced app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -8,45 +8,49 @@ import akka.kafka.scaladsl.{Consumer, Producer}
 import akka.stream.scaladsl.{Sink, Source}
 
 import org.apache.kafka.clients.producer.ProducerRecord
-import org.slf4j.LoggerFactory
 
 import scala.collection.mutable
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 import scala.language.postfixOps
 import scala.util.{Failure, Success}
+import scala.io.StdIn
 
 object App extends EmbeddedKafka {
   def main(args: Array[String]): Unit = {
-    val logger = LoggerFactory.getLogger(getClass)
-
     val conf = new Conf()
 
     implicit val kafkaConfig = EmbeddedKafkaConfig.defaultConfig
     val kafka = EmbeddedKafka.start()
-    logger.info("*** embedded kafka started")
+    println("*** embedded kafka started")
 
     implicit val system = ActorSystem.create("akka-streams-kafka", conf.config)
     implicit val dispatcher = system.dispatcher
-    logger.info("*** akka system started")
+    println("*** akka system started")
 
+    println("*** producer producing records ...")
     val producerDone = Source(1 to 10)
       .map(integer => integer.toString)
       .map(integer => new ProducerRecord[String, String](conf.topic, 0, integer, integer))
       .runWith(Producer.plainSink(conf.producerSettings))
     Await.result(producerDone, 10 seconds)
-    logger.info("*** Producer finished.")
+    println("*** producer finished.")
 
+    println("*** consumer consuming records ...")
     val consumerDone = Consumer
       .plainSource(conf.consumerSettings, conf.subscriptions)
       .runWith(Sink.foreach(println))
     Thread.sleep(10000)
-    logger.info("*** Consumer finished.")
+    println("*** consumer finished.")
+
+    println(s"*** once consumer records are printed, depress RETURN key to stop app.")
+
+    StdIn.readLine()
 
     Await.result(system.terminate(), 30 seconds)
-    logger.info("*** akka system stopped")
+    println("*** akka system stopped")
 
     kafka.stop(false)
-    logger.info("*** embedded kafka stopped")
+    println("*** embedded kafka stopped")
   }
 }
