3ffbdbe9109d868181d1ffd7eea507b62bcae923
objektwerks <objektwerks@runbox.com>
2023-04-12 09:03:26 -0400

    enhanced app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -3,12 +3,17 @@ package objektwerks
 import io.github.embeddedkafka.EmbeddedKafka
 
 import akka.actor.ActorSystem
+import akka.Done
+import akka.kafka.scaladsl.Producer
+import akka.stream.scaladsl.Source
 
+import org.apache.kafka.clients.producer.ProducerRecord
 import org.slf4j.LoggerFactory
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 import scala.language.postfixOps
+import scala.util.{Failure, Success}
 
 object App extends EmbeddedKafka {
   def main(args: Array[String]): Unit = {
@@ -22,6 +27,16 @@ object App extends EmbeddedKafka {
     implicit val system = ActorSystem.create("akka-streams-kafka", conf.config)
     implicit val dispatcher = system.dispatcher
     logger.info("*** akka system started")
+
+    val producerDone = Source(1 to 100)
+      .map(value => new ProducerRecord[String, String](conf.topic, value.toString))
+      .runWith(Producer.plainSink(conf.producerSettings))
+
+    producerDone onComplete  {
+      case Success(_) => logger.info("*** Producer done!")
+      case Failure(error) => println(s"*** Producer failed: ${error.getMessage}")
+    }
+
 
     Await.result(system.terminate(), 30 seconds)
     logger.info("*** akka system stopped")
