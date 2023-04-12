0706a0b1da35403fce11ca5733b35220b481884d
objektwerks <objektwerks@runbox.com>
2023-04-12 08:34:41 -0400

    refactored conf

--- src/main/scala/objektwerks/Conf.scala
+++ src/main/scala/objektwerks/Conf.scala
@@ -14,26 +14,25 @@ import scala.io.Source
 
 final class Conf {
   val config = ConfigFactory.load("app.conf")
-  val producerConfig = config.getConfig("akka.kafka.producer")
-  val consumerConfig = config.getConfig("akka.kafka.consumer")
-  val committerConfig = config.getConfig("akka.kafka.committer")
-  val topic = config.getString("kafka.topic")
-
   val properties = loadProperties("/kafka.properties")
 
+  val producerConfig = config.getConfig("akka.kafka.producer")
   val producerSettings = ProducerSettings[String, String](producerConfig, new StringSerializer, new StringSerializer)
     .withBootstrapServers(producerConfig.getString("bootstrap.servers"))
 
+  val consumerConfig = config.getConfig("akka.kafka.consumer")
   val consumerSettings = ConsumerSettings[String, String](consumerConfig, new StringDeserializer, new StringDeserializer)
     .withBootstrapServers(consumerConfig.getString("bootstrap.servers"))
     .withGroupId(consumerConfig.getString("group.id"))
     .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, consumerConfig.getString("auto.offset.reset"))
 
+  val committerConfig = config.getConfig("akka.kafka.committer")
   val committerSettings = CommitterSettings(committerConfig)
 
+  val topic = config.getString("kafka.topic")
   val subscriptions = Subscriptions.topics(topic)
 
-  def loadProperties(file: String): Properties = {
+  private def loadProperties(file: String): Properties = {
     val properties = new Properties()
     properties.load(Source.fromInputStream(getClass.getResourceAsStream(file)).bufferedReader())
     properties
