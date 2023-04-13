6050a82e370ff3ecee9c38f86c6587a0fbe458a1
objektwerks <objektwerks@runbox.com>
2023-04-12 23:13:16 -0400

    edited sum actor

--- src/main/scala/objektwerks/SumActor.scala
+++ src/main/scala/objektwerks/SumActor.scala
@@ -9,10 +9,10 @@ final case class Add(i: Int) extends Accumulator
 final case object Sum extends Accumulator
 
 final class SumActor extends Actor {
-  val acc = new AtomicInteger(0)
+  val sum = new AtomicInteger(0)
 
   override def receive = {
-    case Add(i) => acc.addAndGet(i)
-    case Sum    => println(s"*** Accumulator Actor Sum: $acc")
+    case Add(i) => sum.addAndGet(i)
+    case Sum    => println(s"*** Accumulator Actor Sum: $sum")
   }
 }
