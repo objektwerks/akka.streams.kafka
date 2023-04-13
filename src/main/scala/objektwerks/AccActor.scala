d35886b05e0bd905d2defb124e9ede044eea2ad9
objektwerks <objektwerks@runbox.com>
2023-04-12 23:18:02 -0400

    edited acc actor

--- src/main/scala/objektwerks/AccActor.scala
+++ src/main/scala/objektwerks/AccActor.scala
@@ -13,6 +13,6 @@ final class AccActor extends Actor {
 
   override def receive = {
     case Add(i) => acc.addAndGet(i)
-    case Sum    => println(s"*** Accumulator Actor Sum: $acc")
+    case Sum    => println(s"*** Acc Actor sum: $acc")
   }
 }
