fe7c6c84af2293ac040ff30fbcc6404708cf2331
objektwerks <objektwerks@runbox.com>
2023-04-14 08:16:57 -0400

    simplified acc actor

--- src/main/scala/objektwerks/AccActor.scala
+++ src/main/scala/objektwerks/AccActor.scala
@@ -4,8 +4,7 @@ import akka.actor.Actor
 
 import java.util.concurrent.atomic.AtomicInteger
 
-sealed trait Adder
-final case class Add(i: Int) extends Adder
+final case class Add(i: Int)
 
 final class AccActor extends Actor {
   val acc = new AtomicInteger(0)
