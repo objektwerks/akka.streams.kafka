9231cb0b808ddcc999f0c94b64ba86aa4fca1969
objektwerks <objektwerks@runbox.com>
2023-04-14 08:31:15 -0400

    edited accumulator and app

--- src/main/scala/objektwerks/Accumulator.scala
+++ src/main/scala/objektwerks/Accumulator.scala
@@ -4,14 +4,14 @@ import akka.actor.Actor
 
 import java.util.concurrent.atomic.AtomicInteger
 
-final case class Add(partition: Int, integer: Int)
+final case class Add(partition: Int, key: String, value: Int)
 
 final class Accumulator extends Actor {
   val acc = new AtomicInteger(0)
 
   override def receive = {
-    case Add(partition, integer) =>
-      acc.addAndGet(integer)
-      println(s"*** Accumulator partition: $partition add: $integer sum: $acc")
+    case Add(partition, key, value) =>
+      acc.addAndGet(value)
+      println(s"*** Accumulator partition: $partition key: $key value: $value sum: $acc")
   }
 }
