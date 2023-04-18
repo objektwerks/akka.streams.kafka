38b1ff55d2f5df8aebc8d315f71bf48f968848bf
objektwerks <objektwerks@runbox.com>
2023-04-18 19:33:37 -0400

    edited logging

--- src/main/scala/objektwerks/ActorApp.scala
+++ src/main/scala/objektwerks/ActorApp.scala
@@ -25,7 +25,7 @@ final class Worker(partition: Int) extends Actor with ActorLogging {
 
   def receive: Receive = {
     case work @ Work(partition, offset, key, value) => // Note worker name vis-a-vis id and partition in app.log!
-      log.info(s"*** worker name: ${context.self.path.name} id: $partition partition: ${partition} offset: ${offset} key: ${key} value: ${value}")
+      log.info(s"*** name: ${context.self.path.name} id: $partition partition: ${partition} offset: ${offset} key: ${key} value: ${value}")
   }
 }
 
